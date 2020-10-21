a4aa5b5f8a1f82ea7b2e90541a41f54da8f7f6e2
Sebastian Jimenez <sebastianjimenez@Sebastians-MacBook-Air.local>
2020-10-20 21:16:15 -0600

    Updates

---
+++ precache-manifest.d2630337fcd81de2d5626314c461890e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f1e3642972176cf95d7dfba710ecc55",
+    "url": "/budget/index.html"
+  },
+  {
+    "revision": "88cfdb7c20a666677854",
+    "url": "/budget/static/css/main.67361308.chunk.css"
+  },
+  {
+    "revision": "8d5181464a1bb5ccecd6",
+    "url": "/budget/static/js/2.423045ef.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/budget/static/js/2.423045ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88cfdb7c20a666677854",
+    "url": "/budget/static/js/main.5073c756.chunk.js"
+  },
+  {
+    "revision": "8c622cafbe88fe3a0c60",
+    "url": "/budget/static/js/runtime-main.66124bb8.js"
+  }
+]);
